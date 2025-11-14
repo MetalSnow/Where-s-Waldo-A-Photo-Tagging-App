76ed9fc9060c018b25c671beb8fea33a6fd2afd5
MetalSnow <aymanetrend@gmail.com>
2025-11-14 23:22:34 +0800

    test: Add integration test for grtting photos route

--- backend/tests/photo.test.js
+++ backend/tests/photo.test.js
@@ -2,8 +2,15 @@ const { app, request } = require('./setup');
 
 test('photo route works', (done) => {
   request(app)
-    .get('/')
+    .get('/photos')
+    .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect({ images: ['img1', 'img2', 'img3'] })
+    .expect((res) => {
+      const photos = res.body.photos;
+      expect(Array.isArray(photos)).toBe(true);
+      expect(photos.length).toBeGreaterThan(0);
+      expect(photos[0]).toHaveProperty('name');
+      expect(photos[0]).toHaveProperty('fileUrl');
+    })
     .expect(200, done);
 });
