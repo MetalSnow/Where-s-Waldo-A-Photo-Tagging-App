8e89d804c05d81f70e1e7272351dea26750e1706
MetalSnow <aymanetrend@gmail.com>
2026-01-04 18:11:47 +0800

    feat: Modularize functionalities and style scenarios
    - separate fetching functionality into reusable hook useFetch
    - separate scenarios into its own component
    - finish styling scenarios images and spans

---
+++ frontend/src/components/scenarios/Scenarios.jsx
@@ -0,0 +1,34 @@
+import { LoaderCircle } from 'lucide-react';
+import useFetch from '../../hooks/useFetch';
+import styles from './Scenarios.module.css';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const Scenarios = () => {
+  const { data, loading, error } = useFetch(`${API_BASE_URL}/photos`);
+
+  if (error) return <p>A network error was encountered</p>;
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <LoaderCircle size={30} strokeWidth={2.5} className={styles.loader} />
+      ) : (
+        <ul>
+          {data.map((scenario) => {
+            return (
+              <li key={scenario.id}>
+                <img
+                  src={API_BASE_URL + scenario.fileUrl}
+                  alt={scenario.name}
+                />
+                <span>{scenario.name} scenario</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Scenarios;
---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const getData = useCallback(async () => {
+    setError(null);
+    try {
+      const response = await fetch(url);
+      if (response.status >= 400) {
+        throw new Error('server error!');
+      }
+
+      const resData = await response.json();
+      setData(resData.photos);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
